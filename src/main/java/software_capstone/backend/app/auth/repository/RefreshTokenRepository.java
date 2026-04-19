03b52bbbed73ce2161ea1a0949c922c07a8361b2
Park, Yeonji <yeonpyj23@gmail.com>
2026-04-19 23:06:48 +0900

    refactor(#1): @Repository 추가

--- src/main/java/software_capstone/backend/app/auth/repository/RefreshTokenRepository.java
+++ src/main/java/software_capstone/backend/app/auth/repository/RefreshTokenRepository.java
@@ -1,10 +1,12 @@
 package software_capstone.backend.app.auth.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import software_capstone.backend.app.auth.document.RefreshToken;
 
 import java.util.Optional;
 
+@Repository
 public interface RefreshTokenRepository extends MongoRepository<RefreshToken, String> {
 
     Optional<RefreshToken> findByToken(String token);
