d69af01c54e9652a09be1b1b0129ed4c572eb5d3
Park, Yeonji <yeonpyj23@gmail.com>
2026-04-19 21:35:02 +0900

    feat(#1): 다큐먼트 클래스 추가

---
+++ src/main/java/software_capstone/backend/app/auth/document/OAuthProvider.java
@@ -0,0 +1,5 @@
+package software_capstone.backend.app.auth.document;
+
+public enum OAuthProvider {
+    KAKAO, NAVER
+}
---
+++ src/main/java/software_capstone/backend/app/auth/document/User.java
@@ -0,0 +1,19 @@
+package software_capstone.backend.app.auth.document;
+
+import lombok.*;
+import org.springframework.data.mongodb.core.mapping.Document;
+import software_capstone.backend.global.document.BaseEntity;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "users")
+public class User extends BaseEntity {
+
+    private OAuthProvider oauthProvider;
+    private String oauthId;
+    private String name;
+    private Role role;
+    private boolean isProActive;
+}
