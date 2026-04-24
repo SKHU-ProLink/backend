c4439909d152853bca203bf963346cb51bd8884c
Park, Daekyeong <pdk2037@gmail.com>
2026-04-25 00:38:59 +0900

    refactor(#9): `User`와 `Auth`를 구분짓기 위해 `User`와 관련한 파일을 `user` 패키지로 분리

--- src/main/java/software_capstone/backend/app/user/document/User.java
+++ src/main/java/software_capstone/backend/app/user/document/User.java
@@ -1,4 +1,4 @@
-package software_capstone.backend.app.auth.document;
+package software_capstone.backend.app.user.document;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 import software_capstone.backend.app.abocado.document.Difficulty;
+import software_capstone.backend.app.auth.document.OAuthProvider;
 import software_capstone.backend.global.document.BaseEntity;
 
 @Getter
