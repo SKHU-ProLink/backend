32e9c1295de69d0d932f18ed59ec1e69585b9ab2
bomikim0214 <rlaqhals05@naver.com>
2026-05-04 01:37:26 +0900

    chore(#18): 상점 아이템 목록 조회 controller 반환 타입 및 인증 파라미터 수정

--- src/main/java/software_capstone/backend/app/store/controller/ShopController.java
+++ src/main/java/software_capstone/backend/app/store/controller/ShopController.java
@@ -2,12 +2,13 @@ package software_capstone.backend.app.store.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import software_capstone.backend.app.store.document.category.ItemCategory;
-import software_capstone.backend.app.store.document.ShopItem;
+import software_capstone.backend.app.store.dto.ShopItemResponse;
 import software_capstone.backend.app.store.service.ShopService;
 
 import java.util.List;
@@ -20,8 +21,10 @@ public class ShopController {
     private final ShopService shopService;
 
     @GetMapping("/items")
-    public ResponseEntity<List<ShopItem>> getItems(
-            @RequestParam(required = false) ItemCategory category) {
+    public ResponseEntity<List<ShopItemResponse>> getItems(
+            @AuthenticationPrincipal String userId,
+            @RequestParam(required = false) ItemCategory category
+    ) {
         return ResponseEntity.ok(shopService.getItems(category));
     }
 }
