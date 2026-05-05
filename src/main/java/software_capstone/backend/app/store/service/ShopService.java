50d6b9f85e5140126248c6f20fa3d089721b79d1
bomikim0214 <rlaqhals05@naver.com>
2026-05-05 18:06:58 +0900

    feat(#18): ShopService 유저 검증 로직 추가

--- src/main/java/software_capstone/backend/app/store/service/ShopService.java
+++ src/main/java/software_capstone/backend/app/store/service/ShopService.java
@@ -6,16 +6,22 @@ import software_capstone.backend.app.store.document.category.ItemCategory;
 import software_capstone.backend.app.store.document.ShopItem;
 import software_capstone.backend.app.store.dto.ShopItemResponse;
 import software_capstone.backend.app.store.repository.ShopItemRepository;
+import software_capstone.backend.app.user.repository.UserRepository;
+import software_capstone.backend.global.exception.ErrorMessage;
+import software_capstone.backend.global.exception.NotFoundException;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ShopService {
-
     private final ShopItemRepository shopItemRepository;
+    private final UserRepository userRepository;
 
-    public List<ShopItemResponse> getItems(ItemCategory category) {
+    public List<ShopItemResponse> getItems(String userId, ItemCategory category) {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException(ErrorMessage.USER_NOT_FOUND));
+
         List<ShopItem> items = (category == null)
                 ? shopItemRepository.findByIsReleasedTrue()
                 : shopItemRepository.findByCategoryAndIsReleasedTrue(category);
