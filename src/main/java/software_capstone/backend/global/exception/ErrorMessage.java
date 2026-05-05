a1c5d4ce5a94386de9d69abc4f4730d7ee27a76c
bomikim0214 <rlaqhals05@naver.com>
2026-05-05 18:04:41 +0900

    chore: ErrorMessage ITEM_NOT_FOUND 추가

--- src/main/java/software_capstone/backend/global/exception/ErrorMessage.java
+++ src/main/java/software_capstone/backend/global/exception/ErrorMessage.java
@@ -11,7 +11,10 @@ public enum ErrorMessage {
     // JWT
     INVALID_TOKEN("유효하지 않은 토큰입니다."),
     EXPIRED_TOKEN("만료된 토큰입니다."),
-    REFRESH_TOKEN_NOT_ALLOWED("리프레시 토큰으로는 접근할 수 없습니다.");
+    REFRESH_TOKEN_NOT_ALLOWED("리프레시 토큰으로는 접근할 수 없습니다."),
+
+    // store
+    ITEM_NOT_FOUND("아이템을 찾을 수 없습니다.");
 
     private final String message;
 
