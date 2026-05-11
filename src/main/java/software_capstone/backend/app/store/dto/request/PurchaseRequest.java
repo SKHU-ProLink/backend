989086b1127893eae72918bd38a23cce0b67afce
bomikim0214 <rlaqhals05@naver.com>
2026-05-11 23:29:24 +0900

    feat(#21): 아이템 구매 요청/응답 PurchaseRequest, PurchaseResponse 추가

---
+++ src/main/java/software_capstone/backend/app/store/dto/request/PurchaseRequest.java
@@ -0,0 +1,10 @@
+package software_capstone.backend.app.store.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PurchaseRequest {
+    private String itemId;
+}
