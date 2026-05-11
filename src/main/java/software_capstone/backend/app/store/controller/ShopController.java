29e66cc8dbbd01e7fb99fee01a615ddba8324a4e
bomikim0214 <rlaqhals05@naver.com>
2026-05-11 23:40:37 +0900

    feat(#21): ShopController 아이템 구매 POST API 추가

--- src/main/java/software_capstone/backend/app/store/controller/ShopController.java
+++ src/main/java/software_capstone/backend/app/store/controller/ShopController.java
@@ -3,11 +3,10 @@ package software_capstone.backend.app.store.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import software_capstone.backend.app.store.document.category.ItemCategory;
+import software_capstone.backend.app.store.dto.request.PurchaseRequest;
+import software_capstone.backend.app.store.dto.response.PurchaseResponse;
 import software_capstone.backend.app.store.dto.response.ShopItemResponse;
 import software_capstone.backend.app.store.service.ShopService;
 
@@ -27,5 +26,14 @@ public class ShopController implements ShopControllerDocs {
             @RequestParam(required = false) ItemCategory category
     ) {
         return ResponseEntity.ok(shopService.getItems(userId, category));
+    }
+
+    @Override
+    @PostMapping("/items/purchase")
+    public ResponseEntity<PurchaseResponse> purchaseItem(
+            @AuthenticationPrincipal String userId,
+            @RequestBody PurchaseRequest request
+    ) {
+        return ResponseEntity.ok(shopService.purchaseItem(userId, request));
     }
 }
