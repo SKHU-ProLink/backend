2519b67413283432eea2b41a53ef06d06f3993fa
bomikim0214 <rlaqhals05@naver.com>
2026-05-13 15:53:00 +0900

    feat(#42): InventoryResponse 추가

---
+++ src/main/java/software_capstone/backend/app/store/dto/response/InventoryResponse.java
@@ -0,0 +1,30 @@
+package software_capstone.backend.app.store.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import software_capstone.backend.app.store.document.ShopItem;
+import software_capstone.backend.app.store.document.UserInventory;
+import software_capstone.backend.app.store.document.category.ItemCategory;
+import software_capstone.backend.app.store.document.category.ItemGrade;
+
+@Getter
+@Builder
+public class InventoryResponse {
+    private String inventoryId;
+    private String itemId;
+    private String itemName;
+    private ItemCategory category;
+    private ItemGrade grade;
+    private int quantity;
+
+    public static InventoryResponse of(UserInventory inventory, ShopItem item) {
+        return InventoryResponse.builder()
+                .inventoryId(inventory.getId())
+                .itemId(item.getId())
+                .itemName(item.getName())
+                .category(item.getCategory())
+                .grade(item.getGrade())
+                .quantity(inventory.getQuantity())
+                .build();
+    }
+}
