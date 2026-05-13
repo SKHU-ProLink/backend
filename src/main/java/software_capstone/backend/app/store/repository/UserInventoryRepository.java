dd290980e82e23f81be903ea1ea77b699b66961d
bomikim0214 <rlaqhals05@naver.com>
2026-05-13 15:58:10 +0900

    feat(#42): UserInventoryRepository findByUserId 메서드 추가

--- src/main/java/software_capstone/backend/app/store/repository/UserInventoryRepository.java
+++ src/main/java/software_capstone/backend/app/store/repository/UserInventoryRepository.java
@@ -3,8 +3,10 @@ package software_capstone.backend.app.store.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import software_capstone.backend.app.store.document.UserInventory;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserInventoryRepository extends MongoRepository<UserInventory, String> {
     Optional<UserInventory> findByUserIdAndItemId(String userId, String itemId);
+    List<UserInventory> findByUserId(String userId);
 }
