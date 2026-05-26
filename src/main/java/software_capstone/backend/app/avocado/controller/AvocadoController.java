aff7044563d61caa1e18e2e8d3e25dd848618a95
Park, Daekyeong <pdk2037@gmail.com>
2026-05-26 17:13:19 +0900

    chore(#48): 불필요한 import 제거

--- src/main/java/software_capstone/backend/app/avocado/controller/AvocadoController.java
+++ src/main/java/software_capstone/backend/app/avocado/controller/AvocadoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.w3c.dom.ranges.RangeException;
 import software_capstone.backend.app.avocado.dto.AvocadoCreateRequest;
 import software_capstone.backend.app.avocado.dto.AvocadoOnboardingRequest;
 import software_capstone.backend.app.avocado.service.AvocadoService;
