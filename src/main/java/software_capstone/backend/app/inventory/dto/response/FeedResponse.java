aa0d68e1cc50bbfe9e5bd56f12a93f084f3c8419
bomikim0214 <rlaqhals05@naver.com>
2026-05-29 15:36:16 +0900

    feat(#26): InventoryControllerDocs 아이템 급여 Swagger 명세 추가

--- src/main/java/software_capstone/backend/app/inventory/dto/response/FeedResponse.java
+++ src/main/java/software_capstone/backend/app/inventory/dto/response/FeedResponse.java
@@ -2,6 +2,7 @@ package software_capstone.backend.app.inventory.dto.response;
 
 import lombok.Builder;
 import lombok.Getter;
+import software_capstone.backend.app.avocado.document.Avocado;
 import software_capstone.backend.app.inventory.document.UserInventory;
 
 @Getter
@@ -11,11 +12,11 @@ public class FeedResponse {
     private int currentExp;
     private String currentLevel;
 
-    public static FeedResponse of(UserInventory inventory, Abocado abocado) {
+    public static FeedResponse of(UserInventory inventory, Avocado avocado) {
         return FeedResponse.builder()
                 .remainingQuantity(inventory.getQuantity())
-                .currentExp(abocado.getExp())
-                .currentLevel(abocado.getLevel().name())
+                .currentExp(avocado.getExp())
+                .currentLevel(avocado.getLevel().name())
                 .build();
     }
 }
